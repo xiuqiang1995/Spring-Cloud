480d2103dcb2b04587f573c68f88ec6419cc4653
quintin <a18805819821@gmail.com>
2020-06-20 20:27:04 +0800

    admin

---
+++ admin-test/src/main/java/com/example/admintest/AdminTestApplication.java
@@ -0,0 +1,21 @@
+package com.example.admintest;
+
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+import de.codecentric.boot.admin.server.domain.entities.InstanceRepository;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+@EnableAdminServer
+public class AdminTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdminTestApplication.class, args);
+    }
+
+    @Bean
+    public DingDingNotifier dingDingNotifier(InstanceRepository repository) {
+        return new DingDingNotifier(repository);
+    }
+}
